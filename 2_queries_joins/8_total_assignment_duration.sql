0c680256bd8691e1d462d98a6ff38e4e5df2c5e5
Michael Hallett <m.hallett@gmail.com>
2020-05-25 22:32:14 +0000

    Removed file. Contents moved to 8_students_total_assignment_duration.sql

--- 2_queries_joins/8_total_assignment_duration.sql
+++
@@ -1,5 +0,0 @@
-SELECT sum(assignment_submissions.duration) as total_duration
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'FEB12';
